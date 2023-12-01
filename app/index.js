7953395ad3b4427c5c5e3223c9407e3b3f717ebc
neillouis3 <neil03.castillon@gmail.com>
2023-12-01 09:17:10 -0330

    Styling

--- app/index.js
+++ app/index.js
@@ -1,10 +1,19 @@
 import react from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Centra</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
