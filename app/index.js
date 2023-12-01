0a90d4670823f12cf8c9fb1bf2859ab1b528967e
neillouis3 <neil03.castillon@gmail.com>
2023-12-01 09:15:44 -0330

    Changes on index.js

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+    return (
+        <View>
+            <Text>Centra</Text>
+        </View>
+    );
+}
