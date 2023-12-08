f4cd6e95e286e16f038c29f4ecc6e2476e584f97
neillouis3 <neil03.castillon@gmail.com>
2023-12-07 21:23:18 -0330

    Layout

--- app/(tabs)/settings/_layout.tsx
+++ app/(tabs)/settings/_layout.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Stack } from "expo-router";
 
 export default function Layout({  }) {
-
+    return (
+        <Stack.Navigator>
+        <Stack.Screen name="Settings" component={Settings} />
+        </Stack.Navigator>
+    );
 }
 
