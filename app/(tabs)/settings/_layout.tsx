2542b3634ae462cbcbfedeff3f9176c413ab28a5
neillouis3 <neil03.castillon@gmail.com>
2023-12-08 23:55:42 -0330

    Styling

--- app/(tabs)/settings/_layout.tsx
+++ app/(tabs)/settings/_layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Stack } from "expo-router";
 import StyleSheets from "react-native";
+import { View } from "react-native";
 
 export default function Layout({  }) {
     return (
         <Stack>
         <Stack.Screen name="Settings" />
+            <View></View>
         </Stack>
     );
 
