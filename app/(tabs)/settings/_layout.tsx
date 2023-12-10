4785fbc8bf4d6f6f090ae08cbb54147278b7f5e7
neillouis3 <neil03.castillon@gmail.com>
2023-12-09 20:53:35 -0330

    Styling

--- app/(tabs)/settings/_layout.tsx
+++ app/(tabs)/settings/_layout.tsx
@@ -22,8 +22,7 @@ export default function Layout({  }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
+        backgroundColor: "#fff",        alignItems: "center",
         justifyContent: "center",
     },
 });
