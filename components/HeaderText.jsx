1367deab069931795f85ba2b7031aac69bd97660
neillouis3 <neil03.castillon@gmail.com>
2023-12-20 23:46:14 -0330

    Styling

--- components/HeaderText.jsx
+++ components/HeaderText.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import { Text, View } from "react-native";
 import { StyleSheet } from "react-native";
 
-
 const HeaderText = (props) => {
-    return (
-      <View><Text style={[props.style, { color: props.textColor }]}>{props.text}</Text></View>
-  
-    );
-  }
-  
+	return (
+		<View>
+			<Text style={[props.style, { color: props.textColor }]}>
+				{props.text}
+			</Text>
+		</View>
+	);
+};
 
-  export default HeaderText;
-  
- 
+export default HeaderText;
